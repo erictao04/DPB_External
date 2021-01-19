c0f22e8ba4d2b6a5522ed0d700b4e1178b1260e7
Eric Tao <erictao04@gmail.com>
2021-01-18 20:29:11 -0500

    01.18.2021

---
+++ run_analyser.py
@@ -0,0 +1,8 @@
+from DPB_Strategy_External import Stock, analyse_stock, analyse_all
+
+'''
+Stock is class of stock analyser
+analyze_stock --> insert single argument(ticker), can change rest of keyword
+                  arguments to change testing strategy
+analyse_all   --> analyses all stocks that you have its stock data
+'''
